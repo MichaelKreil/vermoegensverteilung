f3c85be97330ee5c88a5a1e7d6a4571d9f1c70bf
Micho <github@michael-kreil.de>
2013-09-01 15:07:49 +0200

    renaming partials

--- generator/config.js
+++ generator/config.js
@@ -3,12 +3,16 @@ module.exports = {
 		minified:false
 	},
 	partials: [
-		'1_selbsteinschaetzung',
-		'2_grunddaten',
-		'3_analyse',
-		'4_vermoegenssteuer'
+		'a_selbsteinschaetzung',
+		'b_grunddaten',
+		'c_analyse',
+		'd_vermoegenssteuer'
 	],
 	phrases: {
-		html_title:{ de:'Vermögensverteilung' }
+		html_title:{ de:'Vermögensverteilung' },
+		a_header:{ de:'Wie schätzen Sie sich ein?'},
+		b_header:{ de:'Wie hoch ist Ihr Vermögen?'},
+		c_header:{ de:'So stehen sie da?'},
+		d_header:{ de:'Gewinnen sie bei einer Vermögenssteuer?' },
 	}
 };
